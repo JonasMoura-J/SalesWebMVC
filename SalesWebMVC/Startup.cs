fcf2e7e009954fe9a9009862ec5f1251f741d28f
JonasMoura-J <jonasmourat9090@gmail.com>
2021-06-08 09:48:41 -0300

    Adicionando injeção de dependências para popular banco de dados

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -30,14 +30,17 @@ namespace SalesWebMVC
             services.AddDbContext<SalesWebMVCContext>(options =>
                     options.UseNpgsql(Configuration.GetConnectionString("SalesWebMVCContext"), ApplicationBuilder =>
                     ApplicationBuilder.MigrationsAssembly("SalesWebMVC")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seeding)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seeding.Seed();
             }
             else
             {
