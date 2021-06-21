18226638341e91ed8f89ad7e1b91104147d5c534
JonasMoura-J <jonasmourat9090@gmail.com>
2021-06-21 07:36:07 -0300

    Adicionando tela e controller de cadastro

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 using System;
 using System.Collections.Generic;
@@ -21,9 +22,17 @@ namespace SalesWebMVC.Controllers
             return View(list);  
         }
 
-        public IActionResult create()
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _selerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
